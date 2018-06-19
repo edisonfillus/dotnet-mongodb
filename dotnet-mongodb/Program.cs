3a3115af799af5f7632200592a041a11b6b8fd92
Edison Klafke Fillus <edison.fillus@gmail.com>
2018-06-18 23:18:54 -0300

    Include example of Projections

--- dotnet-mongodb/Program.cs
+++ dotnet-mongodb/Program.cs
@@ -11,7 +11,9 @@ class Program
         //InsertOne.InsertOneExample().GetAwaiter().GetResult();
         //Find.FindExample().GetAwaiter().GetResult();
         //FindFilter.FindFilterObjectMapExample().GetAwaiter().GetResult();
-        FindSkipLimitSort.FindSkipLimitSortBsonExample().GetAwaiter().GetResult();
+        //FindSkipLimitSort.FindSkipLimitSortBsonExample().GetAwaiter().GetResult();
+        FindProjection.FindProjectionBsonExample().GetAwaiter().GetResult();
+
 
         Console.WriteLine("Press Enter");
         Console.ReadLine();
